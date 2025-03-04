63b8068edc19200518dc0032b1273d5afa6f2177
Laura <laura.laukiz@gmail.com>
2025-03-04 09:43:08 +0100

    creando num aleatorio

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
 'use strict'
+
+const inputNumber = document.querySelector(".js-input-number");
+const btn = document.querySelector(".js-btn");
+const pista = document.querySelector(".js-pista");
+const tries = document.querySelector(".js-tries");
+
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+    
+  }
+const randomNumber = getRandomInt(100);
+console.log(randomNumber)
+
+
+btn.addEventListener("click", (ev) => {
+    ev.preventDefault();
+    const numberValue = inputNumber.value
+    console.log(numberValue);
+    
+})
+
+ 
