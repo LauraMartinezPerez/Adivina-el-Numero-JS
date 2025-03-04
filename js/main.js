8360dbe509c521a6c1e3f0afde668001453345a2
Laura <laura.laukiz@gmail.com>
2025-03-04 10:22:39 +0100

    ejercicio completado

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const inputNumber = document.querySelector(".js-input-number");
 const btn = document.querySelector(".js-btn");
 const pista = document.querySelector(".js-pista");
 const tries = document.querySelector(".js-tries");
+let contador = 0;
 
 
 
@@ -18,8 +19,24 @@ console.log(randomNumber)
 btn.addEventListener("click", (ev) => {
     ev.preventDefault();
     const numberValue = inputNumber.value
+    
     console.log(numberValue);
+
+   contador = contador + 1;
+
+    if (numberValue < 1 || numberValue > 100) {
+        pista.innerHTML = "El número debe estar entre 1 y 100"
+    } else {
+        if (numberValue > randomNumber) {
+            pista.innerHTML = "Demasiado alto"
+        } else if (numberValue < randomNumber) {
+            pista.innerHTML = "Demasiado bajo"
+        } else {pista.innerHTML = "Has ganado campeona!"}
+    }    
+    
+    tries.innerHTML = `Número de intentos: ${contador}`;
     
 })
+
 
  
